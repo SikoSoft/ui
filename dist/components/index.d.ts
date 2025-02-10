15ece31210c914afbebeae44d93e9119f4519d5b
Aaron Wright <aaron@sikosoft.com>
2025-02-10 18:51:37 +0100

    1.6.0 update folder structure of components

--- dist/components/index.d.ts
+++ dist/components/index.d.ts
@@ -1,7 +1,7 @@
-export * as SSButton from './ss-button';
-export * as SSCollapsable from './ss-collapsable';
-export * as SSInputAuto from './ss-input-auto';
-export * as SSInput from './ss-input';
-export * as SSLoader from './ss-loader';
-export * as SSSelect from './ss-select';
-export * as SSToggle from './ss-toggle';
+export * as SSButton from './ss-button/ss-button';
+export * as SSCollapsable from './ss-collapsable/ss-collapsable';
+export * as SSInputAuto from './ss-input-auto/ss-input-auto';
+export * as SSInput from './ss-input/ss-input';
+export * as SSLoader from './ss-loader/ss-loader';
+export * as SSSelect from './ss-select/ss-select';
+export * as SSToggle from './ss-toggle/ss-toggle';
