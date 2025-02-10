9541273a407b105da301a43d5dc88aae1df5938d
Aaron Wright <aaron@sikosoft.com>
2025-02-10 18:18:12 +0100

    minor styling tweaks to ss-button component

--- src/components/ss-button.ts
+++ src/components/ss-button.ts
@@ -11,16 +11,24 @@ export class SSButton extends LitElement {
   static styles = [
     theme,
     css`
-      .positive {
-        background-color: var(--positive-background-color);
-        color: var(--positive-color);
-      }
-      .negative {
-        background-color: var(--negative-background-color);
-        color: var(--negative-color);
-      }
-      button.disabled {
-        opacity: 0.5;
+      button {
+        border-radius: 0.5rem;
+
+        &.positive {
+          background-color: var(--positive-background-color);
+          color: var(--positive-color);
+          border-color: var(--positive-color);
+        }
+
+        &.negative {
+          background-color: var(--negative-background-color);
+          color: var(--negative-color);
+          border-color: var(--negative-color);
+        }
+
+        &.disabled {
+          opacity: 0.5;
+        }
       }
     `,
   ];
