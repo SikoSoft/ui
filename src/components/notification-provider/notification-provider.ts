713f2053b852bc5f9e107262ef9af9593010c77c
Aaron Wright <aaron@sikosoft.com>
2025-03-25 22:53:12 +0100

    cleanup unnecessary code in notification-provider

--- src/components/notification-provider/notification-provider.ts
+++ src/components/notification-provider/notification-provider.ts
@@ -26,11 +26,6 @@ export class NotificationProvider extends LitElement {
         top: 0;
         right: 0;
         z-index: 1000;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-end;
-        gap: 0.5rem;
-        padding: 1rem;
       }
     `,
   ];
