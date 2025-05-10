43fb3a300fa5d98bfe533c3d6f908ebea60b665f
Aaron Wright <aaron@sikosoft.com>
2025-05-10 09:25:05 +0200

    update tag-manager stories with additional data and use case demonstration

--- src/components/tag-manager/tag-manager.stories.ts
+++ src/components/tag-manager/tag-manager.stories.ts
@@ -4,7 +4,11 @@ import { html } from 'lit';
 import { StoryBook } from '../../lib/StoryBook';
 
 import './tag-manager';
-import { TagManagerProps, tagManagerProps } from './tag-manager.models';
+import {
+  TagManagerProp,
+  TagManagerProps,
+  tagManagerProps,
+} from './tag-manager.models';
 
 const argTypes = StoryBook.buildArgTypes(tagManagerProps);
 
@@ -16,10 +20,46 @@ const meta = {
   },
   args: {
     value: '',
+    enableSuggestions:
+      tagManagerProps[TagManagerProp.ENABLE_SUGGESTIONS].default,
   },
   argTypes,
-  render: args => html` <tag-manager></tag-manager>> `,
+  render: args => html`
+    <tag-manager
+      value=${args.value}
+      ?enableSuggestions=${args.enableSuggestions}
+    >
+      <div slot="tags">
+        <data-item>monkey</data-item>
+        <data-item>tiger</data-item>
+      </div>
+      <div slot="suggestions">
+        <data-item>apple</data-item>
+        <data-item>cat</data-item>
+        <data-item>banana</data-item>
+        <data-item>dog</data-item>
+        <data-item>grape</data-item>
+        <data-item>kiwi</data-item>
+        <data-item>lemon</data-item>
+        <data-item>mango</data-item>
+        <data-item>orange</data-item>
+        <data-item>peach</data-item>
+      </div>
+    </tag-manager>
+  `,
 } satisfies Meta<TagManagerProps>;
 
 export default meta;
 type Story = StoryObj<TagManagerProps>;
+
+export const WithSuggestions: Story = {
+  args: {
+    enableSuggestions: true,
+  },
+};
+
+export const WithoutSuggestions: Story = {
+  args: {
+    enableSuggestions: false,
+  },
+};
