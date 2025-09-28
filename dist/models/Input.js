622e967c736540297d21028c250404a8e93903cc
Aaron Wright <aaron@sikosoft.com>
2025-09-28 14:40:16 +0200

    remove dist/models/Input* files

--- dist/models/Input.js
+++
@@ -1,9 +0,0 @@
-export var InputType;
-(function (InputType) {
-    InputType["TEXT"] = "text";
-    InputType["DATE"] = "date";
-    InputType["DATETIME_LOCAL"] = "datetime-local";
-    InputType["PASSWORD"] = "password";
-    InputType["NUMBER"] = "number";
-})(InputType || (InputType = {}));
-//# sourceMappingURL=Input.js.map
