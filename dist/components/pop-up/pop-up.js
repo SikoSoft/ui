624520485345f4306865b71a62756fbeb0fb56c9
Aaron Wright <aaron@sikosoft.com>
2025-11-15 20:36:27 +0100

    2.16.0 update pop-up to emit opened event

--- dist/components/pop-up/pop-up.js
+++ dist/components/pop-up/pop-up.js
@@ -10,7 +10,7 @@ import { customElement, property, query, state } from 'lit/decorators.js';
 import { theme } from '../../styles/theme';
 import { PopUpProp, popUpProps } from './pop-up.models';
 import { classMap } from 'lit/directives/class-map.js';
-import { PopUpClosedEvent } from './pop-up.events';
+import { PopUpClosedEvent, PopUpOpenedEvent } from './pop-up.events';
 let PopUp = class PopUp extends LitElement {
     constructor() {
         super(...arguments);
@@ -101,6 +101,7 @@ let PopUp = class PopUp extends LitElement {
         super.updated(_changedProperties);
         if (_changedProperties.has(PopUpProp.OPEN)) {
             if (this[PopUpProp.OPEN]) {
+                this.dispatchEvent(new PopUpOpenedEvent({}));
                 this.newlyOpened = true;
                 setTimeout(() => {
                     this.newlyOpened = false;
